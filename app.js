99f1347aa619fe8419d16de04a58f0e97062745c
Anupama_Halder <anupamahalder2022@gmail.com>
2024-06-10 11:15:39 +0530

    Added the count value of items is added to cart

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const displayProducts = (products) =>{
         div.innerHTML = `
             <img src=${product.image} alt=""/>
             <h5>${product.title.slice(0, 40)}</h5>
-            <p>${product.description.slice(0, 70)}</p>
+            <p>${product.description.slice(0, 50)}</p>
             <p><b>Price:</b> ${product.price} /-</p>
             <button>Details</button>
             <button onclick="handleAddToCart('${product.title?.slice(0,20)}', ${product.price})">Add to Cart</button>
@@ -26,14 +26,28 @@ const displayProducts = (products) =>{
 }
 
 const handleAddToCart = (name, price) =>{
+    const cartCount = document.getElementById("count-item").innerText;
+    let convertedCount = parseInt(cartCount);
+    convertedCount += 1;
+    document.getElementById("count-item").innerText = convertedCount;
     const container = document.getElementById("cart-main-container");
     // create a div 
     const div = document.createElement("div");
     div.classList = "card-info";
     div.innerHTML = `
     <p>${name}, </p>
-    <p><b>Price:</b> Rs. ${price}</p>
+    <p><b>Price: Rs. <span class="price">${price}</span></b></p>
     `
     container.appendChild(div);
     console.log(name, price);
+    updateTotal();
+}
+
+const updateTotal = ()=>{
+    const allPrice = document.getElementsByClassName("price");
+    let total = 0;
+    for(const element of allPrice){
+        total += parseFloat(element.innerText);
+    }
+    document.getElementById("total-price").innerText = " Rs. " + total.toFixed(2);
 }
